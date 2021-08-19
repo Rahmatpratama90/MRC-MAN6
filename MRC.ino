fa714edb9688f32063aceda46dd4e9866af45686
Rahmatpratama90 <47783115+Rahmatpratama90@users.noreply.github.com>
2021-08-19 10:01:43 +0700

    Update MRC.ino

--- MRC.ino
+++ MRC.ino
@@ -1,12 +1,12 @@
 #include <Wire.h>
-#include "MAX30105.h"
 #include <Adafruit_MLX90614.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+int pos = 0; 
+#include <Wire.h>
+#include "MAX30105.h"
 #include <LiquidCrystal.h>
 #include <Servo.h> //Library servo
 MAX30105 particleSensor; // initialize MAX30102 with I2C
-Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-Servo myservo;
 #define USEFIFO
 uint32_t ir, red , green;
 double fred, fir;
@@ -24,55 +24,27 @@ byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
 int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
 int pulseWidth = 69; //Options: 69, 118, 215, 411
 int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+Servo myservo;
 
-void setup()
-{
-  Serial.begin(115200);
-  // Initialize sensor
-  particleSensor.begin(Wire, I2C_SPEED_FAST); //Use default I2C port, 400kHz speed
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
-  lcd.begin(16, 2); //inisialisasi lcd
-  mlx.begin(); //mlx nya di begin
-  myservo.attach(8); //servo nya di attach dengan mengambil pin 8 sebagai pin nya
-  pinMode(10, OUTPUT); //pinmOde 10-13 sebagai outputnya
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13,OUTPUT);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit MLX90614 test");  
+  mlx.begin();  
+  myservo.attach(8);
 }
- 
-void loop() {  //masuk ke program utama
-  particleSensor.check(); //Check the sensor
-  red = particleSensor.getFIFORed(); //why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
-  ir = particleSensor.getFIFOIR(); //why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
 
-    fred = (double)red;
-    fir = (double)ir;
-    avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
-    aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
-    sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
-    sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
+void loop() {
 
-    double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
-    // Serial.println(R);
-    SpO2 = 112 - (17*R);  //http://ww1.microchip.com/downloads/jp/AppNotes/00001525B_JP.pdf
-    particleSensor.nextSample(); //We're finished with this sample so move to next sample
-    Serial.println(SpO2);
-    //Serial.println(",");Serial.println(R);
-
-
+  
   celsius = mlx.readObjectTempC();
   celsius = celsius + 2;
-  lcd.setCursor(0, 0);
-  lcd.print("SuhuBdn :");
-  lcd.print(celsius);
-  lcd.print("C"); 
+  Serial.print("Celcius sekarang adalah : ");
+  Serial.print(celsius);
 
-  lcd.setCursor(0, 1);
-  lcd.print("SpO2    :");
-  lcd.print(SpO2);
-  lcd.print("%");
 
   if (celsius <= 35.3){
+    Serial.println("Suhu dibawah 35.3 :");
+     Serial.println(celsius);
     myservo.write(-15);
     digitalWrite(10, HIGH);
     digitalWrite(11, LOW);
@@ -82,6 +54,8 @@ void loop() {  //masuk ke program utama
 
   if (celsius <= 37.6 && celsius >= 35.3){
     myservo.write(75);
+    Serial.println("Suhu diantara 35.3 s/d 37.6 :");
+     Serial.println(celsius);
     delay(15);
     digitalWrite(10, LOW);
     digitalWrite(11, HIGH);//LED BLUE
@@ -91,13 +65,15 @@ void loop() {  //masuk ke program utama
   }
 
   if(celsius >=37.7) {
+    Serial.println("Suhu diatas 37.7 :" );
+    Serial.println(celsius);
     myservo.write(-15);
     delay(15);
     digitalWrite(10, LOW);  
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
     digitalWrite(13, HIGH);
-    delay(15);
-  }
 
-  }  
+  }
+  delay(500);
+}
